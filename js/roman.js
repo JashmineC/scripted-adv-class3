4770301ba9e0ea7472d0315db9901bc5e81b0fd4
Jashmine <jashminec7459@tywls.org>
2016-10-25 21:59:53 +0000

    my comment

--- js/roman.js
+++ js/roman.js
@@ -16,13 +16,89 @@ $(function() {
   // 4   | IV
   // 1   | I
 
-  function convertToRomanNumeral(number) {
+ function convertToRomanNumeral(number) {
     console.log("Number = " + number);
     var output = "";
-    return output;
-  }
-
-
+  //  if (number == 1) {
+    //  output = "I";
+   // }
+   // else if (number == 2) {
+   //   output = "II";
+   // }
+   // else if (number == 3) {
+    //  output = "III";
+   // }
+  //  else if (number == 4) {
+   //   output = "IV";
+   // }
+    //else if (number == 5) {
+     // output = "V";
+   // }
+   // else if (number == 6) {
+     // output = "VI";
+   // }
+  //   else if (number == 7) {
+  //     output ="VII";
+  //   }
+  //   else if (number == 8) {
+  //     output = "VIII";
+  //   }
+  //   else if (number == 9) {
+  //     output = "IX";
+  //   }
+  //   else if (number == 10) {
+  //     output = "X";
+  //   }
+  //   else {
+  //     output = ":'(";
+  //   }
+  //   return output;
+  // }
+  
+ var output = ""; 
+ while ( number > 0) {
+   console.log(number)
+   if (number >= 100) {
+     output += "C";
+     number-=100;
+   }
+   else if (number >= 90) {
+     output += "XC";
+     number-= 90;
+   }
+   else if (number >= 50) {
+     output +="L"
+     number-= 50;
+   }
+   else if  (number >= 40) {
+     output += "XL"
+     number-= 40;
+   }
+   else if (number >= 10) {
+     output += "X"
+     number-= 10;
+   }
+   else if (number >= 9) {
+     output += "IX"
+     number-= 9;
+   }
+   else if (number >= 5) {
+     output += "V"
+     number-= 5;
+   }
+   else if (number >= 4) {
+     output += "IV"
+     number-= 4;
+   }
+   else if (number >= 1) {
+     output += "I"
+     number-= 1;
+   }
+ } 
+ return output
+ }
+ 
+ 
 
   //-----------------------------------------------------------------------------------
   // Convert the number input when the button is clicked
